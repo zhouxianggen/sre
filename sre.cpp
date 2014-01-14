92259180a244202069119148711c46920def2731
zhouxg <zhouxianggen@gmail.com>
2014-01-14 17:01:18 +0800

    first version

--- sre.cpp
+++ sre.cpp
@@ -13,304 +13,304 @@ void Sre::release()
 
 bool Sre::compile(const char* exp)
 {
-	release();
-	if (!exp)
-		return false;
-	_graph = make_graph(exp, 0, strlen(exp), graph_t());
+  release();
+  if (!exp)
+    return false;
+  _graph = make_graph(exp, 0, strlen(exp), graph_t());
   //_graph._first->show();
-	return !_graph.empty();
+  return !_graph.empty();
 }
 
-int	Sre::match(const vector<char_t>& chars, int start)
+int  Sre::match(const vector<char_t>& chars, int start)
 {
   int end = match_graph(chars, start, _graph._first);
-	return end - start;
+  return end - start;
 }
 
 int Sre::match_graph(const vector<char_t>& chars, int posi, node_t* pnode)
 {
   //printf("match, char=%4x, %x, (%2d, %4x)\n", chars[posi]._code, (uint64)pnode, pnode->_token, pnode->_code);
-	switch (pnode->_token) 
-	{
-	case COMMON:
+  switch (pnode->_token) 
+  {
+  case COMMON:
     if (posi >= chars.size() || chars[posi]._code != pnode->_code)
       goto match_failed;
-		posi += 1;
-		break;
-	case POSI_BEGIN:
-		if (posi != 0)
+    posi += 1;
+    break;
+  case POSI_BEGIN:
+    if (posi != 0)
       goto match_failed;
-		break;
-	case POSI_END:
-		if (posi != chars.size())
+    break;
+  case POSI_END:
+    if (posi != chars.size())
       goto match_failed;
-		return posi;
-	case LEFT_PARENTHESE:
-		break;
-	case RIGHT_PARENTHESE:
-		break;
-	case DOT:
+    return posi;
+  case LEFT_PARENTHESE:
+    break;
+  case RIGHT_PARENTHESE:
+    break;
+  case DOT:
     if (posi >= chars.size())
       goto match_failed;
-		posi += 1;
-		break;
-	case ALPHABET:
+    posi += 1;
+    break;
+  case ALPHABET:
     if (posi >= chars.size() || !isalpha(chars[posi]._code))
       goto match_failed;
-		posi += 1;
-		break;
-	case NOT_ALPHABET:
+    posi += 1;
+    break;
+  case NOT_ALPHABET:
     if (posi >= chars.size() || isalpha(chars[posi]._code))
       goto match_failed;
-		posi += 1;
-		break;
-	case CHINESE:
+    posi += 1;
+    break;
+  case CHINESE:
     if (posi >= chars.size() || !ischn(chars[posi]._code))
       goto match_failed;
-		posi += 1;
-		break;
-	case NOT_CHINESE:
+    posi += 1;
+    break;
+  case NOT_CHINESE:
     if (posi >= chars.size() || ischn(chars[posi]._code))
       goto match_failed;
-		posi += 1;
-		break;
-	case NUMBER:
+    posi += 1;
+    break;
+  case NUMBER:
     if (posi >= chars.size() || !isdigit(chars[posi]._code))
       goto match_failed;
-		posi += 1;
-		break;
-	case NOT_NUMBER:
+    posi += 1;
+    break;
+  case NOT_NUMBER:
     if (posi >= chars.size() || isdigit(chars[posi]._code))
       goto match_failed;
-		posi += 1;
-		break;
-	case SPACE:
+    posi += 1;
+    break;
+  case SPACE:
     if (posi >= chars.size() || !isspace(chars[posi]._code))
       goto match_failed;
-		posi += 1;
-		break;
-	case NOT_SPACE:
+    posi += 1;
+    break;
+  case NOT_SPACE:
     if (posi >= chars.size() || isspace(chars[posi]._code))
       goto match_failed;
-		posi += 1;
-		break;
-	default:
-		break;
-	}
-	if (pnode->_nexts.size() == 0)
-		return posi;
-	for (int i=0; i<pnode->_nexts.size(); i+=1) {
-		int end = match_graph(chars, posi, pnode->_nexts[i]);
-		if (end >= posi)
+    posi += 1;
+    break;
+  default:
+    break;
+  }
+  if (pnode->_nexts.size() == 0)
+    return posi;
+  for (int i=0; i<pnode->_nexts.size(); i+=1) {
+    int end = match_graph(chars, posi, pnode->_nexts[i]);
+    if (end >= posi)
       return end;
-	}
+  }
 match_failed:
-	return 0;
+  return 0;
 }
 
-token_t Sre::get_token (const char *exp, int &pos, uint32 &code) 
+token_t Sre::get_token(const char *exp, int &pos, uint32 &code) 
 {
   uint32 clen;
 
   get_utf8_char(exp+pos, clen, code);
-	pos += clen;
+  pos += clen;
 
-	if (clen==1 && char(code)=='\\') {
+  if (clen==1 && char(code)=='\\') {
     get_utf8_char(exp+pos, clen, code);
-		pos += clen;
-		if (clen==1 && char(code)=='a') return ALPHABET;
-		if (clen==1 && char(code)=='c') return CHINESE;
-		if (clen==1 && char(code)=='d') return NUMBER;
-		if (clen==1 && char(code)=='s') return SPACE;
-		if (clen==1 && char(code)=='A') return NOT_ALPHABET;
-		if (clen==1 && char(code)=='C') return NOT_CHINESE;
-		if (clen==1 && char(code)=='D') return NOT_NUMBER;
-		if (clen==1 && char(code)=='S') return NOT_SPACE;
-		if (clen==1 && char(code)=='f') {code = uint32('\f'); return COMMON;}
-		if (clen==1 && char(code)=='n') {code = uint32('\n'); return COMMON;}
-		if (clen==1 && char(code)=='r') {code = uint32('\r'); return COMMON;}
-		if (clen==1 && char(code)=='t') {code = uint32('\t'); return COMMON;}
-		if (clen==1 && char(code)=='v') {code = uint32('\v'); return COMMON;}
-		return COMMON;
-	}
-	if (clen==0)  return END_REGEXP;
-	if (clen==1 && char(code)=='(') return LEFT_PARENTHESE;
-	if (clen==1 && char(code)==')') return RIGHT_PARENTHESE;
-	if (clen==1 && char(code)=='|') return BACKTRACE;
-	if (clen==1 && char(code)=='.') return DOT;
-	if (clen==1 && char(code)=='^' && pos == 1) return POSI_BEGIN;
-	if (clen==1 && char(code)=='$' && !exp[pos]) return POSI_END;
-	if (clen==1 && char(code)=='*') {
-		if (exp[pos] == '?') {pos += 1; return REPEAT_ZERO_MORE;}
-		else return REPEAT_MORE_ZERO;
-	}
-	if (clen==1 && char(code)=='+') {
-		if (exp[pos] == '?') {pos += 1; return REPEAT_ONCE_MORE;}
-		else return REPEAT_MORE_ONCE;
-	}
-	if (clen==1 && char(code)=='?') {
-		if (exp[pos] == '?') {pos += 1; return REPEAT_ZERO_ONCE;}
-		else return REPEAT_ONCE_ZERO;
-	}
-	return COMMON;
+    pos += clen;
+    if (clen==1 && char(code)=='a') return ALPHABET;
+    if (clen==1 && char(code)=='c') return CHINESE;
+    if (clen==1 && char(code)=='d') return NUMBER;
+    if (clen==1 && char(code)=='s') return SPACE;
+    if (clen==1 && char(code)=='A') return NOT_ALPHABET;
+    if (clen==1 && char(code)=='C') return NOT_CHINESE;
+    if (clen==1 && char(code)=='D') return NOT_NUMBER;
+    if (clen==1 && char(code)=='S') return NOT_SPACE;
+    if (clen==1 && char(code)=='f') {code = uint32('\f'); return COMMON;}
+    if (clen==1 && char(code)=='n') {code = uint32('\n'); return COMMON;}
+    if (clen==1 && char(code)=='r') {code = uint32('\r'); return COMMON;}
+    if (clen==1 && char(code)=='t') {code = uint32('\t'); return COMMON;}
+    if (clen==1 && char(code)=='v') {code = uint32('\v'); return COMMON;}
+    return COMMON;
+  }
+  if (clen==0)  return END_REGEXP;
+  if (clen==1 && char(code)=='(') return LEFT_PARENTHESE;
+  if (clen==1 && char(code)==')') return RIGHT_PARENTHESE;
+  if (clen==1 && char(code)=='|') return BACKTRACE;
+  if (clen==1 && char(code)=='.') return DOT;
+  if (clen==1 && char(code)=='^' && pos == 1) return POSI_BEGIN;
+  if (clen==1 && char(code)=='$' && !exp[pos]) return POSI_END;
+  if (clen==1 && char(code)=='*') {
+    if (exp[pos] == '?') {pos += 1; return REPEAT_ZERO_MORE;}
+    else return REPEAT_MORE_ZERO;
+  }
+  if (clen==1 && char(code)=='+') {
+    if (exp[pos] == '?') {pos += 1; return REPEAT_ONCE_MORE;}
+    else return REPEAT_MORE_ONCE;
+  }
+  if (clen==1 && char(code)=='?') {
+    if (exp[pos] == '?') {pos += 1; return REPEAT_ZERO_ONCE;}
+    else return REPEAT_ONCE_ZERO;
+  }
+  return COMMON;
 }
 
-node_t* Sre::alloc_node (int token, uint32 code)
+node_t* Sre::alloc_node(int token, uint32 code)
 {
-	node_t *p = new node_t(token, code);
-	if (p) _collector.push_back(p);
-	return p;
+  node_t *p = new node_t(token, code);
+  if (p) _collector.push_back(p);
+  return p;
 }
 
-node_t* Sre::alloc_node (const node_t &n)
+node_t* Sre::alloc_node(const node_t &n)
 {
-	node_t *p = new node_t(n);
-	if (p) _collector.push_back(p);
-	return p;
+  node_t *p = new node_t(n);
+  if (p) _collector.push_back(p);
+  return p;
 }
 
-graph_t Sre::make_graph (const char *exp, int s, int e, graph_t prev)
+graph_t Sre::make_graph(const char *exp, int s, int e, graph_t prev)
 {
-	graph_t grh;
-	node_t *pn=NULL, *pn2=NULL;
-	int i, j, k, tok;
-	uint32 code;
+  graph_t grh;
+  node_t *pn=NULL, *pn2=NULL;
+  int i, j, k, tok;
+  uint32 code;
 
-	if (s >= e) return prev;
+  if (s >= e) return prev;
   int olds = s;
-	tok = get_token(exp, s, code);
+  tok = get_token(exp, s, code);
   //printf("exp=%s, tok=%2d, code=%4x\n", exp+olds, tok, code);
-	switch (tok) {
-	case POSI_BEGIN:
-	case POSI_END:
-	case COMMON:
-	case DOT:
-	case ALPHABET:
-	case CHINESE:
-	case NUMBER:
-	case SPACE:
-	case NOT_ALPHABET:
-	case NOT_CHINESE:
-	case NOT_NUMBER:
-	case NOT_SPACE:
+  switch (tok) {
+  case POSI_BEGIN:
+  case POSI_END:
+  case COMMON:
+  case DOT:
+  case ALPHABET:
+  case CHINESE:
+  case NUMBER:
+  case SPACE:
+  case NOT_ALPHABET:
+  case NOT_CHINESE:
+  case NOT_NUMBER:
+  case NOT_SPACE:
     pn = alloc_node(tok, code);
-		if (!pn)
+    if (!pn)
       break;
     if (prev._last)
       prev._last->add_next(pn);
-		grh = make_graph(exp, s, e, graph_t(pn, pn));
-		if (!grh.empty())
-			return graph_t(pn, grh._last);
-		break;
-	
-	case LEFT_PARENTHESE:
+    grh = make_graph(exp, s, e, graph_t(pn, pn));
+    if (!grh.empty())
+      return graph_t(pn, grh._last);
+    break;
+  
+  case LEFT_PARENTHESE:
     pn = alloc_node(tok, code);
     pn2 = alloc_node(RIGHT_PARENTHESE, uint32(')'));
     if (!pn || !pn2)
       break;
-		i = s;
-		j = 1;
-		while ((tok = get_token(exp, s, code)) != END_REGEXP) {
-			if (tok == LEFT_PARENTHESE) j++;
-			if (tok == RIGHT_PARENTHESE) j--;
-			if ((tok==BACKTRACE && j==1) || j==0) {
-				grh = make_graph(exp, i, s-1, graph_t());
-				if (grh.empty()) break;
+    i = s;
+    j = 1;
+    while ((tok = get_token(exp, s, code)) != END_REGEXP) {
+      if (tok == LEFT_PARENTHESE) j++;
+      if (tok == RIGHT_PARENTHESE) j--;
+      if ((tok==BACKTRACE && j==1) || j==0) {
+        grh = make_graph(exp, i, s-1, graph_t());
+        if (grh.empty()) break;
         pn->add_next(grh._first);
         grh._last->add_next(pn2);
-				i = s;
+        i = s;
         if (j == 0) break;
-			}
-		}
-		if (tok==END_REGEXP || grh.empty())
-			break;
-		if (prev._last)
+      }
+    }
+    if (tok==END_REGEXP || grh.empty())
+      break;
+    if (prev._last)
       prev._last->add_next(pn);
-		grh = make_graph(exp, s, e, graph_t(pn, pn2));
-		if (!grh.empty())
-			return graph_t(pn, grh._last);
-		break;
-	
-	case REPEAT_ZERO_ONCE:
-	case REPEAT_ONCE_ZERO:
-		if (prev.empty())
-			return make_graph(exp, s, e, prev);
+    grh = make_graph(exp, s, e, graph_t(pn, pn2));
+    if (!grh.empty())
+      return graph_t(pn, grh._last);
+    break;
+  
+  case REPEAT_ZERO_ONCE:
+  case REPEAT_ONCE_ZERO:
+    if (prev.empty())
+      return make_graph(exp, s, e, prev);
     pn = alloc_node(*(prev._first));
     pn2 = alloc_node(EMPTY);
     if (!pn || !pn2)
       break;
-			
-		*(prev._first) = node_t(tok, uint32('?'));
-		if (prev.single())
+      
+    *(prev._first) = node_t(tok, uint32('?'));
+    if (prev.single())
       pn->add_next(pn2);
-		else
+    else
       prev._last->add_next(pn2);
-		
-		if (tok == REPEAT_ZERO_ONCE) 
-		{
+    
+    if (tok == REPEAT_ZERO_ONCE) 
+    {
       prev._first->add_next(pn2);
       prev._first->add_next(pn);
-		} else {
+    } else {
       prev._first->add_next(pn);
       prev._first->add_next(pn2);
-		}
-		grh = make_graph(exp, s, e, graph_t(prev._first, pn2));
-		if (!grh.empty())
-			return graph_t(prev._first, grh._last);
-		break;
+    }
+    grh = make_graph(exp, s, e, graph_t(prev._first, pn2));
+    if (!grh.empty())
+      return graph_t(prev._first, grh._last);
+    break;
 
-	case REPEAT_ZERO_MORE:
-	case REPEAT_MORE_ZERO:
-		if (prev.empty())
-			return make_graph(exp, s, e, prev);
-		pn = alloc_node(*(prev._first));
+  case REPEAT_ZERO_MORE:
+  case REPEAT_MORE_ZERO:
+    if (prev.empty())
+      return make_graph(exp, s, e, prev);
+    pn = alloc_node(*(prev._first));
     pn2 = alloc_node(EMPTY);
     if (!pn || !pn2)
       break;
 
-		*(prev._first) = node_t(tok, uint32('*'));
-		if (prev.single())
+    *(prev._first) = node_t(tok, uint32('*'));
+    if (prev.single())
       pn->add_next(prev._first);
-		else
+    else
       prev._last->add_next(prev._first);
 
-		if (tok == REPEAT_ZERO_MORE) 
-		{
+    if (tok == REPEAT_ZERO_MORE) 
+    {
       prev._first->add_next(pn2);
       prev._first->add_next(pn);
-		} else {
+    } else {
       prev._first->add_next(pn);
       prev._first->add_next(pn2);
-		}
-		grh = make_graph(exp, s, e, graph_t(prev._first, pn2));
-		if (!grh.empty())
-			return graph_t(prev._first, grh._last);
-		break;
+    }
+    grh = make_graph(exp, s, e, graph_t(prev._first, pn2));
+    if (!grh.empty())
+      return graph_t(prev._first, grh._last);
+    break;
 
-	case REPEAT_ONCE_MORE:
-	case REPEAT_MORE_ONCE:
-		if (prev.empty())
-			return make_graph(exp, s, e, prev);
+  case REPEAT_ONCE_MORE:
+  case REPEAT_MORE_ONCE:
+    if (prev.empty())
+      return make_graph(exp, s, e, prev);
     pn = alloc_node(tok, uint32('+'));
     pn2 = alloc_node(EMPTY);
     
     prev._last->add_next(pn);
-		if (tok == REPEAT_ONCE_MORE) 
-		{
+    if (tok == REPEAT_ONCE_MORE) 
+    {
       pn->add_next(pn2);
       pn->add_next(prev._first);
-		} else {
+    } else {
       pn->add_next(prev._first);
       pn->add_next(pn2);
-		}
-		grh = make_graph(exp, s, e, graph_t(prev._first, pn2));
-		if (!grh.empty())
-			return graph_t(prev._first, grh._last);
-		break;
-	default:
-		break;
-	}
-	return graph_t();
+    }
+    grh = make_graph(exp, s, e, graph_t(prev._first, pn2));
+    if (!grh.empty())
+      return graph_t(prev._first, grh._last);
+    break;
+  default:
+    break;
+  }
+  return graph_t();
 }
 
 } // namespace
--- sre.h
+++ sre.h
@@ -16,40 +16,40 @@ namespace ucltp {
 
 // supported wildcards
 enum token_t {
-	NUMBER, // \d 0	
-	SPACE, // \s 1
-	ALPHABET, // \a 2
-	CHINESE, // \c 3
-	NOT_NUMBER,	// \D 4
-	NOT_SPACE, // \S 5
-	NOT_ALPHABET,	// \A 6
-	NOT_CHINESE, // \C 7
-	POSI_BEGIN, // ^ 8
-	POSI_END, // $ 9
-	LEFT_PARENTHESE, // ( 10
-	RIGHT_PARENTHESE, // ) 11
-	BACKTRACE, // | 12
-	DOT, // . 13
-	REPEAT_ONCE_ZERO, // ? 14
-	REPEAT_MORE_ZERO, // * 15
-	REPEAT_MORE_ONCE,	// + 16
-	REPEAT_ZERO_ONCE, // ?? 17
-	REPEAT_ZERO_MORE, // *?	18
-	REPEAT_ONCE_MORE, // +? 19
-	COMMON, // common 20
-	END_REGEXP, // 0 21
-	EMPTY // null 22
+  NUMBER, // \d 0  
+  SPACE, // \s 1
+  ALPHABET, // \a 2
+  CHINESE, // \c 3
+  NOT_NUMBER,  // \D 4
+  NOT_SPACE, // \S 5
+  NOT_ALPHABET,  // \A 6
+  NOT_CHINESE, // \C 7
+  POSI_BEGIN, // ^ 8
+  POSI_END, // $ 9
+  LEFT_PARENTHESE, // ( 10
+  RIGHT_PARENTHESE, // ) 11
+  BACKTRACE, // | 12
+  DOT, // . 13
+  REPEAT_ONCE_ZERO, // ? 14
+  REPEAT_MORE_ZERO, // * 15
+  REPEAT_MORE_ONCE,  // + 16
+  REPEAT_ZERO_ONCE, // ?? 17
+  REPEAT_ZERO_MORE, // *?  18
+  REPEAT_ONCE_MORE, // +? 19
+  COMMON, // common 20
+  END_REGEXP, // 0 21
+  EMPTY // null 22
 };
 
-/* graph node */
+// graph node
 struct node_t {
-	int _token;
+  int _token;
   uint32 _code;
   vector<node_t*> _nexts;
   bool _show;
-	
+  
   node_t(int tok, uint32 code=0) : _token(tok), _code(code), _show(false) {}
-	void add_next(node_t* p) { _nexts.push_back(p); }
+  void add_next(node_t* p) { _nexts.push_back(p); }
   
   void show(int lev=0) {
     for (int i=0; i<lev; i+=1) printf("--");
@@ -62,31 +62,32 @@ struct node_t {
   }
 };
 
+// graph
 struct graph_t {
-	node_t *_first;
-	node_t *_last;
-	
+  node_t *_first;
+  node_t *_last;
+  
   graph_t(node_t *pf=0, node_t *pl=0): _first(pf), _last(pl) {}
-	bool empty() { return _first == NULL; }
-	bool single() { return _first == _last; }
+  bool empty() { return _first == NULL; }
+  bool single() { return _first == _last; }
 };
 
-class	Sre {
+class Sre {
 public:
-	Sre () {}
-	bool compile(const char *st);
-	int match(const vector<char_t>& chars, int start);
-	~Sre () { release(); }
+  Sre() {}
+  bool compile(const char *st);
+  int match(const vector<char_t>& chars, int start);
+  ~Sre() { release(); }
 private:
-	graph_t _graph;
-	vector<node_t*> _collector;
-	
+  graph_t _graph;
+  vector<node_t*> _collector;
+  
   void release();
   token_t get_token(const char *str, int &pos, uint32& code);
-	graph_t make_graph(const char *str, int start, int end, graph_t prev);
-	int	match_graph (const vector<char_t>& chars, int posi, node_t* pnode);
-	node_t* alloc_node (int token, uint32 code=0);
-	node_t* alloc_node (const node_t &n);
+  graph_t make_graph(const char *str, int start, int end, graph_t prev);
+  int  match_graph (const vector<char_t>& chars, int posi, node_t* pnode);
+  node_t* alloc_node(int token, uint32 code=0);
+  node_t* alloc_node(const node_t &n);
 };
 
 }
